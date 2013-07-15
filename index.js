5a0df44e41dbaa4e9159439268f71c911b7af735
Tim <secoif@gmail.com>
2013-07-15 14:58:51 +0800

    Add diagnostics button.

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ var ecstatic = require('ecstatic')(__dirname + '/public')
 var log = require('debug')(process.env['DEBUG'])
 var status = require('./lib/status')
 var restart = require('./lib/restart')
+var diagnostics = require('./lib/diagnostics')
 
 module.exports = function(program) {
 
   var app = http.createServer(function(req, res) {
-    if (req.url === '/restart.html') restart(program.pid, req, res)
+    if (req.url === '/restart') restart(program.pid, req, res)
+    else if (req.url === '/diagnostics') diagnostics(req, res)
     else ecstatic(req, res)
   })
   // middlewares
